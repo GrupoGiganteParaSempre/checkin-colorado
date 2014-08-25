bba5020ed2df98d7adbf278a34fe796d8b58952a
Gustavo Seganfredo <gustavosf@gmail.com>
2014-08-25 17:54:49 -0300

    Alterações no layout novamente

--- app/src/main/java/me/gpsbr/check_in/LoginActivity.java
+++ app/src/main/java/me/gpsbr/check_in/LoginActivity.java
@@ -111,7 +111,7 @@ public class LoginActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.login, menu);
         return true;
     }
 
@@ -189,6 +189,9 @@ public class LoginActivity extends Activity {
             });
     }
 
+    /**
+     * Simply shows toast
+     */
     private void showToast(CharSequence text) {
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
@@ -355,6 +358,7 @@ public class LoginActivity extends Activity {
 
             if (success) {
                 showToast("Identificação aceita");
+                // Chama CheckinActivity
                 finish();
             } else {
                 if (errorId == 1) {
