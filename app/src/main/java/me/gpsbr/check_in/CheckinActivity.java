7467165d7b6732080e9ceafe9a00b9249ec14806
Gustavo Seganfredo <gustavosf@gmail.com>
2014-08-25 22:11:23 -0300

    Trabalhando no layout da tela de checkin

--- app/src/main/java/me/gpsbr/check_in/CheckinActivity.java
+++ app/src/main/java/me/gpsbr/check_in/CheckinActivity.java
@@ -1,6 +1,8 @@
 package me.gpsbr.check_in;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,8 +30,22 @@ public class CheckinActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        if (id == R.id.action_logout) {
+            // Clear login/password references
+            SharedPreferences.Editor editor = credentials.edit();
+            editor.putString("registration_number", "");
+            editor.putString("password", "");
+            editor.commit();
+
+            // Go back to the main activity (LoginActivity)
+            Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else if (id == R.id.action_about) {
+            // Go to AboutActivity
+            // Intent intent = new Intent(LoginActivity.this, AboutActivity.class);
+            // startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
